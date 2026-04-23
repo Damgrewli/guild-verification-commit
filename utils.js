988c1efd8bcacf5043b517aa5c39238441d2f13e
Damgrewli <44634456+Damgrewli@users.noreply.github.com>
2026-04-24 02:38:37 +0300

    Introduce real-time notifications

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-23T23:38:21.529Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-04-23T23:38:37.483Z
+Commit: Introduce real-time notifications
