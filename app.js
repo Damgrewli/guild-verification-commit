61ab2d0003ac9287c9f65bd7f2f1d254beadc4de
Damgrewli <44634456+Damgrewli@users.noreply.github.com>
2026-04-24 02:38:21 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-23T23:38:16.482Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-04-23T23:38:20.629Z
+Commit: Fix CSS styling bug on mobile devices
