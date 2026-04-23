9eb422197b4729972e0f23f66756501a4203d935
Damgrewli <44634456+Damgrewli@users.noreply.github.com>
2026-04-24 02:39:09 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-23T23:38:54.711Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-04-23T23:39:09.307Z
+Commit: Resolve null pointer exception in user service
